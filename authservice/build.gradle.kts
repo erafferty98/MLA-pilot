cda03ce8bdd9b04aac8d60fcccc11d3d60982d81
Emma Rafferty <erafferty98@gmail.com>
2024-03-30 17:16:09 +0000

    updated dependency

--- authservice/build.gradle.kts
+++ authservice/build.gradle.kts
@@ -20,7 +20,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.passay:passay:1.6.0") // Add Passay dependency
-	implementation ("org.apache.commons:commons-validator:1.7.0") // Corrected Commons Validator dependency
+	implementation ("org.apache.commons:commons-validator:1.8.0") // Corrected Commons Validator dependency
 	implementation ("org.springframework.boot:spring-boot-starter-oauth2-client") // Add Spring OAuth2 dependency
 	implementation("javax.validation:validation-api:2.0.1.Final") // Add javax.validation dependency
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
