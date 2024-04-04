5475ea046e1ca2ea80da9a6374f33aae809964ff
Emma Rafferty <erafferty98@gmail.com>
2024-04-04 09:23:05 +0100

    Update AuthApplication.java
    added suppress warnings

--- authservice/src/main/java/com/authservice/auth/AuthApplication.java
+++ authservice/src/main/java/com/authservice/auth/AuthApplication.java
@@ -17,7 +17,7 @@ public class AuthApplication {
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
 			@Override
-			public void addCorsMappings(CorsRegistry registry) {
+			public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
 				registry.addMapping("/**")
 						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 						.allowedHeaders("*")
