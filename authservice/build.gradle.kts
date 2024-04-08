b7ece474a19183f3692c667276467e27ff2c1b37
Emma Rafferty <erafferty98@gmail.com>
2024-04-08 23:11:35 +0100

    package updates

--- authservice/build.gradle.kts
+++ authservice/build.gradle.kts
@@ -18,6 +18,7 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
 	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("commons-validator:commons-validator:1.8.0")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
